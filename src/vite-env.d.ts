80cb5dd52253047d324b77fce3398a45d14c42b2
Yusuke Kohno <ykstudio626@gmail.com>
2026-05-22 16:59:43 +0900

    デモモード

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,5 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_MAINTENANCE_MODE?: string;
+  readonly VITE_DEMO_MODE?: string;
+  readonly VITE_DEMO_MODE_RESTRICTION?: string;
   readonly EVENT_LOG?: string;
 }
